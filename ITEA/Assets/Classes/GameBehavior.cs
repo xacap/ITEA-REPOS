6d6ce8224a32d6c74fd6fc9fe4e8efb31c7f16e6
xacap <xacap.seo@gmail.com>
2021-04-06 17:52:47 +0300

    init commit

---
+++ ITEA/Assets/Classes/GameBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehavior : MonoBehaviour
+{
+    public GameObject player;
+
+
+    void Start()
+    {
+        GameObject robot = GameObject.Instantiate(player) as GameObject;
+        robot.transform.position = Vector3.zero;
+    }
+
+}
