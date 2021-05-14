e6f6dc8fc6c5ac622a9f6f1ace53a7aa6a20a9d7
xacap <xacap.seo@gmail.com>
2021-05-14 18:40:12 +0300

    hp bar end

---
+++ JunkoChan/Assets/Scripts/Enemy/EnemyControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyControl : MonoBehaviour
+    {
+        public GameObject enemyCanvasGo;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform.CompareTag("Potato"))
+            {
+                enemyCanvasGo.GetComponent<EnemyHpBar>().Dmg();
+            }
+        }
+    }
+}
+
+
--- JunkoChan/Assets/Scripts/UI/PlayerHpBar.cs
+++ JunkoChan/Assets/Scripts/UI/PlayerHpBar.cs
@@ -7,10 +7,33 @@ namespace UI
 {
     public class PlayerHpBar : MonoBehaviour
     {
-        public Transform player;
+        public static PlayerHpBar Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<PlayerHpBar>();
+                    if (instance == null)
+                    {
+                        var instanceContainer = new GameObject("PlayerHpBar");
+                        instance = instanceContainer.AddComponent<PlayerHpBar>();
+                    }
+                }
+                return instance;
+            }
+        }
+        private static PlayerHpBar instance;
+
         public Slider hpBar;
+        public Transform player;
         public float maxHp;
         public float currentHp;
+
+        public GameObject HpLineFolder;
+        public Text playerHpText;
+
+        float unitHp = 200f;
 
         void Start()
         {
@@ -21,7 +44,23 @@ namespace UI
         {
             transform.position = player.position;
             hpBar.value = currentHp / maxHp;
+            playerHpText.text = "" + currentHp;
 
+        }
+
+        public void GetHpBoost()
+        {
+            maxHp += 150;
+            currentHp += 150;
+            float scaleX = (1000f / unitHp) / (maxHp / unitHp);
+            HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(false);
+
+            foreach (Transform child in HpLineFolder.transform)
+            {
+                child.gameObject.transform.localScale = new Vector3(scaleX, 1, 1);
+            }
+
+            HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(true);
         }
     }
 }
